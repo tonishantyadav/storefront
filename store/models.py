ce8e165af8e8e033e5cc7654e93ade73f720370d
Nishant Yadav <inishantyadav24@gmail.com>
2023-06-28 16:10:31 +0530

    Added data validation of product inventory

--- store/models.py
+++ store/models.py
@@ -24,9 +24,9 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
     unit_price = models.DecimalField(
-        max_digits=6, decimal_places=2, validators=[MinValueValidator]
+        max_digits=6, decimal_places=2, validators=[MinValueValidator(1)]
     )
-    inventory = models.IntegerField()
+    inventory = models.IntegerField(validators=[MinValueValidator(1)])
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     slug = models.SlugField()
