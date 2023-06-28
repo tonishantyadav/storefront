63e6baf3ed3601ba43e81972b677cd203ebad605
Nishant Yadav <inishantyadav24@gmail.com>
2023-06-28 11:48:02 +0530

    Customizing the admin models list page

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Collection)
-admin.site.register(models.Product)
+
+# Admin model that represents Product model in admin site
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    # Properties that applied on Admin model
+    list_display = ["title", "unit_price"]
+    list_editable = ["unit_price"]
+    list_per_page = 20
+
+
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ["title", "featured_product"]
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "membership"]
+    list_editable = ["membership"]
+    ordering = ["first_name", "last_name"]
+    list_per_page = 20
