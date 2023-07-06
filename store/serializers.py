b0ed8cb15a3dbfa5b7e361be4399b989e912fe74
Nishant Yadav <inishantyadav24@gmail.com>
2023-07-06 17:30:40 +0530

    Rename unit_price to price in ProductSerializer

--- store/serializers.py
+++ store/serializers.py
@@ -1,6 +1,15 @@
+from decimal import Decimal
+
 from rest_framework import serializers
+
+from . import models
+
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
-    unit_price = serializers.DecimalField(6, 2)
+    price = serializers.DecimalField(6, 2, source="unit_price")
+    # price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
+
+    # def calculate_tax(self, product: models.Product):
+    #     return product.unit_price * Decimal(1.1)
