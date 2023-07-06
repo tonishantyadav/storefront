93ff8722e34668db3b9d7290e126af19f23cfb5e
Nishant Yadav <inishantyadav24@gmail.com>
2023-07-06 18:11:18 +0530

    Fixing ProductSerializer

--- store/serializers.py
+++ store/serializers.py
@@ -10,12 +10,14 @@ class CollectionSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class ProductSerializer(serializers.ModelSerializer):
     price = serializers.DecimalField(6, 2, source="unit_price")
     price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
     collection = CollectionSerializer()
 
     def calculate_tax(self, product: models.Product):
         return product.unit_price * Decimal(1.1)
+
+    class Meta:
+        model = models.Product
+        fields = ["id", "title", "price", "price_with_tax", "collection"]
