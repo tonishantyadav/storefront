ac045b832d23c070d4647c2d57d2b301e4dc57f1
Nishant Yadav <inishantyadav24@gmail.com>
2023-07-06 16:48:57 +0530

    Create product serializer

---
+++ store/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+
+class ProductSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
+    unit_price = serializers.DecimalField(6, 2)
--- store/views.py
+++ store/views.py
@@ -1,6 +1,9 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from . import models
+from . import serializers
+
 
 @api_view()
 def product_list(request):
@@ -9,4 +12,6 @@ def product_list(request):
 
 @api_view()
 def product_detail(request, pk):
-    return Response(pk)
+    product = models.Product.objects.get(pk=pk)
+    serializer = serializers.ProductSerializer(product)
+    return Response(data=serializer.data)
