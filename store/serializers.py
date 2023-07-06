2f4ee3dd892fbc272ae95930b8b33a9d6dcbdf59
Nishant Yadav <inishantyadav24@gmail.com>
2023-07-06 17:31:37 +0530

    Add calculate_tax field to ProductSerializer

--- store/serializers.py
+++ store/serializers.py
@@ -9,7 +9,7 @@ class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
     price = serializers.DecimalField(6, 2, source="unit_price")
-    # price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
+    price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
 
-    # def calculate_tax(self, product: models.Product):
-    #     return product.unit_price * Decimal(1.1)
+    def calculate_tax(self, product: models.Product):
+        return product.unit_price * Decimal(1.1)
