a5452e7dafa86fed44eb841510c38502dfb81765
Nishant Yadav <inishantyadav24@gmail.com>
2023-07-09 14:26:42 +0530

    Implement pagination to all VS of store

--- storefront/settings.py
+++ storefront/settings.py
@@ -139,4 +139,8 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
-REST_FRAMEWORK = {"COERCE_DECIMAL_TO_STRING": False}
+REST_FRAMEWORK = {
+    "COERCE_DECIMAL_TO_STRING": False,
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
