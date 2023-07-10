8419665d64618f1e1bdeb36609f082e6f63646ea
Nishant Yadav <inishantyadav24@gmail.com>
2023-07-10 20:30:49 +0530

    Add user login and logout endpoints

--- store/urls.py
+++ store/urls.py
@@ -5,16 +5,15 @@ from . import views
 
 router = routers.DefaultRouter()
 
+router.register("products", views.ProductViewSet, basename="products")
 router.register("collections", views.CollectionViewSet)
+router.register("carts", views.CartViewSet)
 router.register("customers", views.CustomerViewSet)
 
-router.register("products", views.ProductViewSet, basename="products")
 products_router = routers.NestedDefaultRouter(router, "products", lookup="product")
 products_router.register("reviews", views.ReviewViewSet, basename="product-reviews")
 
-router.register("carts", views.CartViewSet)
 carts_router = routers.NestedDefaultRouter(router, "carts", lookup="cart")
 carts_router.register("items", views.CartItemViewSet, basename="cart-items")
-
 
 urlpatterns = router.urls + products_router.urls + carts_router.urls
