d78b7869cd03119930eaf9e26a4d47b247a45a29
Nishant Yadav <inishantyadav24@gmail.com>
2023-07-11 14:35:19 +0530

    Renaming choice field of Order model

--- store/models.py
+++ store/models.py
@@ -88,13 +88,13 @@ class Address(models.Model):
 
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = "P"
-    PAYMENT_STATUS_Complete = "C"
-    PAYMENT_STATUS_Failed = "F"
+    PAYMENT_STATUS_COMPLETE = "C"
+    PAYMENT_STATUS_FAILED = "F"
 
     PAYMENT_STATUS = [
         (PAYMENT_STATUS_PENDING, "Pending"),
-        (PAYMENT_STATUS_Complete, "Complete"),
-        (PAYMENT_STATUS_Failed, "Failed"),
+        (PAYMENT_STATUS_COMPLETE, "Complete"),
+        (PAYMENT_STATUS_FAILED, "Failed"),
     ]
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(
