20b3ac580212f435f1848eca08e7fe033715f23a
Nishant Yadav <inishantyadav24@gmail.com>
2023-07-12 12:08:34 +0530

    Setting up media

--- storefront/urls.py
+++ storefront/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 admin.site.site_header = "Storefront Admin"
 admin.site.index_title = "Admin"
@@ -29,3 +31,7 @@ urlpatterns = [
     path("playground/", include("playground.urls")),
     path("store/", include("store.urls")),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
