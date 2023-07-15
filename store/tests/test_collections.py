d6be5fba569aea182b13bd538e248bce0ed7cad9
Nishant Yadav <inishantyadav24@gmail.com>
2023-07-15 12:06:08 +0530

    Running test- test_if_user_is_not_authenticate_returns_401

---
+++ store/tests/test_collections.py
@@ -0,0 +1,11 @@
+import pytest
+from rest_framework import status
+from rest_framework.test import APIClient
+
+
+@pytest.mark.django_db
+class TestCreateCollection:
+    def test_if_user_is_not_authenticate_returns_401(self):
+        client = APIClient()
+        response = client.post("/store/collections/", {"title": "a"})
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
