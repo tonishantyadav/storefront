1f90b6d7ada5361a40bf08c3995c2d026dc6d1e9
Nishant Yadav <inishantyadav24@gmail.com>
2023-08-04 11:37:01 +0530

    Serving static files to production

--- storefront/settings.py
+++ storefront/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMidddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
